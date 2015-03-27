31399d2d09c6fbed7f02f3ec15653d42f7c305fd
Nico <nic.girault@gmail.com>
2015-03-27 22:35:34 +0100

    test

--- index.js
+++ index.js
@@ -24,31 +24,6 @@ module.exports = {
         }
     },
 
-    // Extend ebook resources and html
-    website: {
-        assets: "./book",
-        js: [
-            "test.js"
-        ],
-        css: [
-            "test.css"
-        ],
-        html: {
-            "html:start": function() {
-                return "<!-- Start book "+this.options.title+" -->"
-            },
-            "html:end": function() {
-                return "<!-- End of book "+this.options.title+" -->"
-            },
-
-            "head:start": "<!-- head:start -->",
-            "head:end": "<!-- head:end -->",
-
-            "body:start": "<!-- body:start -->",
-            "body:end": "<!-- body:end -->"
-        }
-    },
-
     // Extend templating blocks
     blocks: {
         // Author will be able to write "{% myTag %}World{% endMyTag %}"

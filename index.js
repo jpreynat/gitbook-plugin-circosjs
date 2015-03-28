f77d84131c96a5ddb07e91b7cfd3930927f25efa
Nico <nic.girault@gmail.com>
2015-03-28 11:08:27 +0100

    fix cdn sync

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = {
     // Extend website resources and html
     website: {
         html: {
-            "head:end": "<script src='https://cdn.rawgit.com/nicgirault/circosJS/master/dist/circosJS.js'></script>\n<script src='https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js'></script>\n<link rel='stylesheet' href='https://cdn.rawgit.com/nicgirault/circosJS/master/dist/colorBrewer.css'></link>"
+            "head:end": "<script src='https://raw.githack.com/nicgirault/circosJS/master/dist/circosJS.js'></script>\n<script src='https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js'></script>\n<link rel='stylesheet' href='https://cdn.rawgit.com/nicgirault/circosJS/master/dist/colorBrewer.css'></link>"
         }
     }
 };

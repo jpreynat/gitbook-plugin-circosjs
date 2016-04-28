7fa6345c243bf37bad3e82b48bd7aceb61032a7f
Johan Preynat <johan.preynat@gmail.com>
2016-04-28 17:34:51 +0200

    Include scripts and stylesheets using GitBook 3 templating

--- index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    // Extend website resources and html
-    website: {
-        html: {
-            "head:end": "<script src='https://raw.githack.com/nicgirault/circosJS/master/dist/circosJS.js'></script>\n<script src='https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js'></script>\n<link rel='stylesheet' href='https://cdn.rawgit.com/nicgirault/circosJS/master/dist/colorBrewer.css'></link>"
-        }
-    }
-};
